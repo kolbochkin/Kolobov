8a62b600caa539a7e1f9018b513eed56263778fc
kolbochkin <kolbochkin@gmail.com>
2021-08-09 00:57:31 +0300

    задание 3.3

--- Kolobov_Denis_dz_3.3.py
+++ Kolobov_Denis_dz_3.3.py
@@ -1,5 +1,13 @@
 def thesaurus(*args):
-    #index = {}
-    print(f'args: {args[0]}')
+    index = {}
+    for i in args:
+        z = i[0]
+        print(z)
+        print(index.setdefault(z, i))
+    print(index)
+    #key = tuple(map(str[0], args)
+    #print(f'args: {args}')
+    #print(key)
+
 
 thesaurus("Иван", "Мария", "Петр", "Илья")
